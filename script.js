7de714a14042063d169a57ee34b7caea988215e2
jtopolska <101990409+jtopolska@users.noreply.github.com>
2022-08-26 12:14:19 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+
+gsap.from('.nav-item', {x: 400, delay: .5, duration: 1, opacity: 0, stagger: .5})
+gsap.from('.btn-outline-warning', {x: -1000, rotation: 1440, duration:2, delay: 2, opacity: 0})
+
+
+
